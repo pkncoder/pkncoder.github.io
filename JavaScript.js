41151f6959dfc50142077f557d6c376c5c6663ff
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-05-15 09:58:41 -0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+function ClickShow() {
+  var x = document.getElementById("ClickShows");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function ClickShow1() {
+  var x = document.getElementById("ClickShows1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function ClickShow2() {
+  var x = document.getElementById("ClickShows2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
