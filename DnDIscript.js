cb1fa778e4ed4f5b0a5368a2b2570000410eefe8
pkncoder <alex.h.preston@gmail.com>
2023-12-29 15:12:54 -0500

    Tryyying to get the formatting for the api to work

--- DnDIscript.js
+++ DnDIscript.js
@@ -11,8 +11,7 @@ async function setInputExtraLis(url)
             var li = document.createElement("li");
             var a = document.createElement("a");
             a.appendChild(document.createTextNode(data["results"][i]["name"]));
-            a.setAttribute('id', 'extra-param-li')
-            a.href = 'DnDIData.html?apipath=' + searchParams.get('search') + "/" + data["results"][i]["name"].toLowerCase()
+            a.href = 'DnDIData.html?apipath=' + searchParams.get('search') + "/" + formatForApi(data["results"][i]["name"])
             li.appendChild(a)
             ul.appendChild(li);
         }
@@ -27,13 +26,24 @@ function setupInputPage() {
 }
 
 function searchForUrl() {
-    window.location.href = 'DnDIData.html?apipath=' + searchParams.get('search') + "/" + document.getElementById('data-input-box').value
+    window.location.href = 'DnDIData.html?apipath=' + searchParams.get('search') + "/" + formatForApi(document.getElementById('data-input-box').value)
 }
 
 function getData() {
-    fetch('https://www.dnd5eapi.co/api/' + searchParams.get('apipath'))
+    fetch('https://www.dnd5eapi.co/api/' + formatForApi(searchParams.get('apipath')))
     .then(response => response.json())
     .then(data => {
         document.getElementById('dataResultP').innerHTML = JSON.stringify(data, null, 2);
     })
+}
+
+function formatForApi(unformattedString) {
+    unformattedString = unformattedString.toLowerCase()
+    unformattedString = unformattedString
+    unformattedString = unformattedString.replace(', +1, +2, or +3', '').replace(', +1', '-1').replace(', +2', '-2').replace(', +3', '-3')
+    unformattedString = unformattedString.replace(' × ', 'x').replace('(', '').replace(')', '').replaceAll(' ft.', '')
+    unformattedString = unformattedString.replace(',', '').replace(' form', '').replace("'", '')
+    unformattedString = unformattedString.replaceAll(':', '').replace('/','-')
+    let formattedString = unformattedString.replaceAll(' ', '-')
+    return formattedString
 }
