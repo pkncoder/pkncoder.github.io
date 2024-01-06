a24d701cb6553d0d6d4623aacb90eb44eb31561d
pkncoder <alex.h.preston@gmail.com>
2024-01-06 01:06:59 -0500

    Added some new buttons with a new extras pad

--- CalcScript.js
+++ CalcScript.js
@@ -1,7 +1,31 @@
 const display = document.getElementById("display");
 
+let extrasShown = false;
+
+let closerNum = 0;
+
 function appendToDisplay (item) {
     document.getElementById("display").value += item;
+
+    if (item === 'sqrt(')
+    {
+        closerNum++;
+    }
+
+    else if (item == 'cos(')
+    {
+        closerNum++;
+    }
+
+    else if (item == 'sin(')
+    {
+        closerNum++;
+    }
+
+    else if (item == 'tan(')
+    {
+        closerNum++;
+    }
 }
 
 function clearDisplay () {
@@ -12,7 +36,7 @@ function solve () {
     let ul = document.getElementById("cache-ul");
     let li = document.createElement('li');
 
-    let text = document.createTextNode(document.getElementById("display").value + " = " + eval('with (Math) ' + document.getElementById("display").value))
+    let text = document.createTextNode(document.getElementById("display").value + ')'.repeat(closerNum) + " = " + eval('with (Math) ' + document.getElementById("display").value + ')'.repeat(closerNum)))
 
     li.appendChild(text);
     li.classList.add('cached-item')
@@ -38,4 +62,23 @@ function setupEventListener () {
           solve()
         }
       });
+}
+
+function largenCalc () {
+    let extras = document.getElementById("extras_holder");
+    let main_div = document.getElementById("main_div");
+
+    if (extrasShown)
+    {
+        extras.style.display = 'none';
+        main_div.setAttribute("style", "grid-template-columns: repeat(2, 1fr)");
+        extrasShown = false;
+    }
+
+    else
+    {
+        extras.style.display = 'block';
+        main_div.setAttribute("style", "grid-template-columns: repeat(3, 1fr)");
+        extrasShown = true;
+    }
 }
