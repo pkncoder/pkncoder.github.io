b6b9e8c84f139e1c88b0cd7dbea65a2682e20322
pkncoder <128310642+pkncoder@users.noreply.github.com>
2024-02-01 14:36:16 +0000

    Added the new calculator, need to add it to the navbars

--- CalcScript.js
+++ CalcScript.js
@@ -10,20 +10,6 @@ function clearDisplay () {
     document.getElementById("display").value = '  ';
 }
 
-function solve () {
-    let ul = document.getElementById("cache-ul");
-    let li = document.createElement('li');
-
-    let text = document.createTextNode(document.getElementById("display").value + " = " + eval('with (Math) ' + document.getElementById("display").value))
-
-    li.appendChild(text);
-    li.classList.add('cached-item')
-
-    ul.appendChild(li);
-
-    document.getElementById("display").value = "  " + eval(document.getElementById("display").value);
-}
-
 function clearCache () {
     let ul = document.getElementById('cache-ul');
 
@@ -37,7 +23,7 @@ function setupEventListener () {
         // If the user presses the "Enter" key on the keyboard
         if (event.key === "Enter") {
           // Trigger the button element with a click
-          solve()
+          newSolve();
         }
       });
 }
