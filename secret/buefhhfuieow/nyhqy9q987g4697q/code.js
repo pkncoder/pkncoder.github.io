d9986bf027d6d7455e0bebf01eb267a1c3d8da2b
pkncoder <alex.h.preston@gmail.com>
2024-02-09 19:32:00 -0500

    ?

---
+++ secret/buefhhfuieow/nyhqy9q987g4697q/code.js
@@ -0,0 +1,6 @@
+function generateButtonMap()
+{
+    let controller = document.getElementById("controller").value;
+    let varName = document.getElementById("varName").value;
+    let button = document.getElementById("button").value
+}
