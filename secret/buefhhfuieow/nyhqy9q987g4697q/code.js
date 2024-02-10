b711ecc97cefc61a486e3fcc821dbdc7dcb1ba07
pkncoder <alex.h.preston@gmail.com>
2024-02-09 19:43:21 -0500

    ?

--- secret/buefhhfuieow/nyhqy9q987g4697q/code.js
+++ secret/buefhhfuieow/nyhqy9q987g4697q/code.js
@@ -3,4 +3,6 @@ function generateButtonMap()
     let controller = document.getElementById("controller").value;
     let varName = document.getElementById("varName").value;
     let button = document.getElementById("button").value
+
+    document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
 }
