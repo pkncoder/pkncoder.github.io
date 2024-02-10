494c164aa0085c550e64b835fef6dc23fe85185d
pkncoder <alex.h.preston@gmail.com>
2024-02-09 20:04:49 -0500

    ?

--- secret/buefhhfuieow/nyhqy9q987g4697q/code.js
+++ secret/buefhhfuieow/nyhqy9q987g4697q/code.js
@@ -2,7 +2,53 @@ function generateButtonMap()
 {
     let controller = document.getElementById("controller").value;
     let varName = document.getElementById("varName").value;
-    let button = document.getElementById("button").value
+    let button = document.getElementById("button").value;
+
+    button = button.toUpperCase()
+
+    if (button.search("[ABXY]") || button.search("[LB]") || button.search("[RB]"))
+    {
+        if (button === "A")
+        {
+            button = 1;
+
+            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
+        }
+
+        else if (button === "B")
+        {
+            button = 2;
+
+            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
+        }
+
+        else if (button === "X")
+        {
+            button = 3;
+
+            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
+        }
+
+        else if (button === "Y")
+        {
+            button = 4;
+
+            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
+        }
+
+        else if (button === "LB")
+        {
+            button = 5;
+
+            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
+        }
+
+        else if (button === "RB")
+        {
+            button = 6;
+
+        }
+    }
 
     document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
 }
