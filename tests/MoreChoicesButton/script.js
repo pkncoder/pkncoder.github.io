ce2fd5dae5a580ae211d8fd741ef29d3d5a0a520
pkncoder <alex.h.preston@gmail.com>
2024-02-12 18:58:41 -0500

    Now have 4 templates ready to configure

--- tests/MoreChoicesButton/script.js
+++ tests/MoreChoicesButton/script.js
@@ -1,30 +1,18 @@
-function expandDiv()
+const divIds = ["one", "two", "three", "four"]
+
+function expandDiv(divNumber)
 {
-    let div = document.getElementById("extras-div");
+    let div = document.getElementById("extras-div-" + divIds[divNumber - 1]);
 
-    if (div.style.visibility === "hidden")
-    {
-        div.style.visibility = "visible";
-        div.style.transform = "translate(-50%, -50%)"
-    }
-
-    else
+    if (div.style.visibility === "visible")
     {
         div.style.visibility = "hidden";
         div.style.transform = "translate(-50%, -200%)"
     }
-}
 
-function setupEventListeners()
-{
-    window.addEventListener("keypress", function(event) {
-        
-        if (event.key === "x")
-        {
-            if (this.document.getElementById("extras-div").style.visibility === "visible")
-            {
-                expandDiv();
-            }
-        }
-    });
+    else
+    {
+        div.style.visibility = "visible";
+        div.style.transform = "translate(-50%, -50%)"
+    }
 }
