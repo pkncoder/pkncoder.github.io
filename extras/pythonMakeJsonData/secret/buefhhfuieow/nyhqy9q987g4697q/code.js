f53eff58a7693c4617fdd143c218456b30c2a4f7
pkncoder <alex.h.preston@gmail.com>
2024-02-13 23:31:50 -0500

    ?

--- extras/pythonMakeJsonData/secret/buefhhfuieow/nyhqy9q987g4697q/code.js
+++ extras/pythonMakeJsonData/secret/buefhhfuieow/nyhqy9q987g4697q/code.js
@@ -1,154 +1,25 @@
-function generateButtonMap()
+function generateButtonMappingBasic()
 {
-    let controller = document.getElementById("controllerButton").value;
-    let varName = document.getElementById("varName").value;
-    let button = document.getElementById("button").value;
+    // The controller (XBOX) that is used
+    let controllerName = document.getElementById("controllerButMap").value;
 
-    button = button.toUpperCase()
+    // The button creation data
+    let buttonVarName = document.getElementById("butVarNameButMap").value;
+    let buttonNumber = document.getElementById("buttNumberButMap").value;
 
-    if (button.search("[ABXY]") || button.search("[LB]") || button.search("[RB]"))
-    {
-        if (button === "A")
-        {
-            button = 1;
-
-            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
-        }
-
-        else if (button === "B")
-        {
-            button = 2;
-
-            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
-        }
-
-        else if (button === "X")
-        {
-            button = 3;
-
-            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
-        }
-
-        else if (button === "Y")
-        {
-            button = 4;
-
-            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
-        }
-
-        else if (button === "LB")
-        {
-            button = 5;
-
-            document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
-        }
-
-        else if (button === "RB")
-        {
-            button = 6;
-
-        }
-    }
-
-    document.getElementById("result").value = "private final JoystickButton " + varName + " = new JoysickButton(" + controller + ", " + button + ");";
+    // The return code
+    document.getElementById("answer").value = "public final JoystickButton " + buttonVarName + " = new JoystickButton(" + controllerName + ", " + buttonNumber + ");";
 }
 
-function generatePOVMap()
+function generateButtonMappingFull()
 {
-    let controller = document.getElementById("controllerPOV").value;
-    let varName = document.getElementById("varNamePOV").value;
-    let pov = document.getElementById("povPOV").value;
+    // The controller variable (XBOX)
+    let controllerName = document.getElementById("extraControllerNumButMap").value;
 
-    pov = pov.toUpperCase();
+    // The button creation data
+    let buttonVarName = document.getElementById("extraButtonVarNameButMap").value;
+    let buttonType = document.getElementById("extraButtonNameButMap").value;
 
-    if (pov === "UP")
-    {
-        pov = 0;
-    }
-
-    else if (pov === "RIGHT")
-    {
-        pov = 90;
-    }
-
-    else if (pov === "DOWN")
-    {
-        pov = 180;
-    }
-
-    else if (pov === "LEFT")
-    {
-        pov = 270;
-    }
-
-    document.getElementById("result").value = "private final POVButton " + varName + " = new POVButton(" + controller + ", " + pov + ");";
-}
-
-function generateButtonFunctionter()
-{
-    // Input vales
-    // The var name of the button
-    let buttonName = document.getElementById("buttonNameFuncer").value;
-
-    // The function name
-    let onActivateFunc = document.getElementById("functionOnOnFuncer").value;
-
-    // On press perams (when actuvated)
-    let onTruePerams = document.getElementById("onPressEventPeramsFuncer").value;
-
-    // On press or on hold
-    let pressOrHold = document.getElementById("eventFuncer").value;
-
-    // The var name of your connectoin to your Mechanizms class
-    let functionConnection = document.getElementById("functionConnectionLineFuncer").value;
-
-    // The name of the on false function
-    let onFalseFuncName = document.getElementById("onFalseEventNameFuncer").value;
-
-    // On false press perams
-    let onFalsePerams = document.getElementById("onFalseEventPeramsFuncer").value;
-    
-    // This is our final code line
-    let code = "";
-
-    // Also lowercase the foolean values
-    pressOrHold = pressOrHold.toLowerCase();
-
-
-
-    // Start adding everything we need
-    // This is the stuff we know that we need to add
-    code += buttonName + ".";
-
-    // Next test for if we will use on true or not (then add the rest we need)
-    // If its on press, do dat
-    if (pressOrHold === "press")
-    {  
-        code += "onTrue(new InstantCommand(() -> " + functionConnection + "." + onActivateFunc + "(" + onTruePerams + ")))";
-    }
-
-    // Else, if we are making an on hold, just change the 'onTrue' to 'whileTrue'
-    // Prob a more efficient way, but this is my site, and there are no more ppl, so shut up
-    else if (pressOrHold === "hold")
-    {
-        code += "whileTrue(new InstantCommand(() -> " + functionConnection + "." + onActivateFunc + "(" + onTruePerams +")))";
-    }
-
-
-
-    // Test to see if the on false func name has somthing, if it does then add the on flase stuff, if not then
-    if (onFalseFuncName === "")
-    {
-        code += ";";
-    }
-
-    // If it is however,
-    else
-    {
-        // Add the on flase thing, isn't too hard
-        code += ".onFalse(new InstantCommand(() -> " + functionConnection + "." + onFalseFuncName + "(" + onFalsePerams + ")));";
-    }
-
-    // This is the same as the return line in our function
-    document.getElementById("result").value = code;
+    // The button number
+    let buttonNameOrNum = document.getElementById("extrButtonTypeButMap").value;
 }
