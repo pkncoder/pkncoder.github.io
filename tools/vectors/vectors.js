6b1a544889f4a154bcf64ff36c5d25fd7259caed
pkncoder <alex.h.preston@gmail.com>
2025-04-29 09:33:41 -0400

    Added two vector math

--- tools/vectors/vectors.js
+++ tools/vectors/vectors.js
@@ -11,6 +11,23 @@ class Vector2D {
     normalized(length) {
         return new Vector2D(this.x / length, this.y / length);
     }
+
+    dot(otherVector) {
+        return (this.x * otherVector.x) + (this.y * otherVector.y);
+    }
+
+    angleBetween(otherVector) {
+        let cosTheta = (this.dot(otherVector)) / (Math.sqrt(this.getLengthSquared()) * Math.sqrt(otherVector.getLengthSquared()));
+        return Math.acos(cosTheta);
+    }
+
+    vectorsAreOrthogonal(otherVector) {
+        return (this.dot(otherVector)) === 0.0;
+    }
+
+    vectorsAreParallel(otherVector) {
+        return ((this.x / otherVector.x) === (this.y / otherVector.y));
+    }
 }
 
 function initialTerminalPoint() {
@@ -20,10 +37,23 @@ function initialTerminalPoint() {
     console.log(terminalPoint);
 
     let vector = new Vector2D(parseFloat(terminalPoint[0] - initialPoint[0]), parseFloat(terminalPoint[1] - initialPoint[1]));
-    gatherAllVectorData(vector);
+    gatherAllOneVectorData(vector);
 }
 
-function gatherAllVectorData(vector) {
+function twoVectors() {
+
+    let vectorOneStr = $("#twoVectorOne").val().trim().replaceAll(" ", "").replaceAll("(", "").replaceAll(")", "").split(",");
+    let vectorTwoStr = $("#twoVectorTwo").val().trim().replaceAll(" ", "").replaceAll("(", "").replaceAll(")", "").split(",");
+    console.log(vectorOneStr);
+    console.log(vectorTwoStr);
+
+    let vectorOne = new Vector2D(parseFloat(vectorOneStr[0]), parseFloat(vectorOneStr[1]));
+    let vectorTwo = new Vector2D(parseFloat(vectorTwoStr[0]), parseFloat(vectorTwoStr[1]));
+
+    gatherAllTwoVectorData(vectorOne, vectorTwo);
+}
+
+function gatherAllOneVectorData(vector) {
     /* Needed Data
     Component Form
     Length (Exact & Rounded)
@@ -42,6 +72,40 @@ function gatherAllVectorData(vector) {
         Normalized / Unit Vector: < ${normalizedVector.x.toFixed(3)}, ${normalizedVector.y.toFixed(3)} >
         Normalized / Unit Vector Exact: [ \n\t\t${vector.x.toFixed(3)} / sqrt(${lengthSquared}), \n\t\t${vector.y.toFixed(3)} / sqrt(${lengthSquared})\n\t]
         Linear Combonation: ${vector.x.toFixed(2)}i+${vector.y.toFixed(2)}j
+        `
+    )
+}
+
+function gatherAllTwoVectorData(vectorOne, vectorTwo) {
+    /* Needed Data
+    Lengths
+    Dot
+    */
+
+    let v1LengthSquared = vectorOne.getLengthSquared();
+    let v1Length = Math.sqrt(v1LengthSquared);
+
+    let v2LengthSquared = vectorTwo.getLengthSquared();
+    let v2Length = Math.sqrt(v2LengthSquared);
+
+    let dot = vectorOne.dot(vectorTwo);
+
+    let angleBetween = vectorOne.angleBetween(vectorTwo);
+
+    let vectorsAreOrthogonal = vectorOne.vectorsAreOrthogonal(vectorTwo);
+    let vectorsAreParallel = vectorOne.vectorsAreParallel(vectorTwo);
+
+    alert(
+        `
+        Vector One's Length: sqrt(${v1LengthSquared.toFixed(3)}) | ${v1Length.toFixed(3)}
+        Vector Two's Length: sqrt(${v2LengthSquared.toFixed(3)}) | ${v2Length.toFixed(3)}
+        
+        Dot Product of both vectors: ${dot.toFixed(3)}
+
+        Angle Between both vectors: ${angleBetween.toFixed(3)}rad | ${((180 * angleBetween) / Math.PI).toFixed(3)}deg
+
+        Vectors are Orthogonal (Perpindicular): ${vectorsAreOrthogonal ? "Yes" : "No"}
+        Vectors are Parallel: ${vectorsAreParallel ? "Yes" : "No"}
         `
     )
 }
